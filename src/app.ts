c86ce03f1d8b3d232731c654ec8eaeebf3dc2653
Kevin Potschien <k.potschien@sensory-minds.com>
2024-09-16 09:55:50 +0200

    v1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
 import Phaser from 'phaser';
 
-const game = new Phaser.Game();
+class GameScene extends Phaser.Scene {
+	preload() {
+		this.load.image('coin', './assets/coin.png');
+	}
+
+	create() {
+		this.add
+			.text(this.sys.canvas.width / 2, 300, 'Hello World')
+			.setOrigin(0.5, 0.5);
+
+		this.add.image(this.sys.canvas.width / 2, 250, 'coin');
+	}
+}
+
+const game = new Phaser.Game({
+	type: Phaser.AUTO,
+	scale: {
+		mode: Phaser.Scale.ScaleModes.FIT,
+		autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+	},
+	parent: 'game',
+	width: 800,
+	height: 600,
+	scene: GameScene,
+});
+
+if (process.env.DEBUG === 'true') {
+	console.log('loaded esbuild watch listener');
+	new EventSource('/esbuild').addEventListener('change', () =>
+		location.reload()
+	);
+}
